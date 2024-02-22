e4559150aad9ab34767aeae0233080fdc8f92c52
unknown <sola2014@naver.com>
2024-02-22 09:30:20 +0900

    modify folder

---
+++ src/main/java/com/example/projectnote/ProjectNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectnote/ProjectNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
